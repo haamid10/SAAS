430d7a7afdb66ff4ba413caef3b8c90ff3fbabe6
haamid10 <apdixamiid919@gmail.com>
2024-03-03 16:58:33 +0300

    stacked some whhere

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,13 @@
+import { Sidebar } from 'lucide-react'
 import React from 'react'
 
 const Layout = ({children}:{children: React.ReactNode}) => {
   return (
         <main className='root'>
+          {/* sidebar */}
+          <Sidebar/>
+          {/* Mobile navigation */}
+
             <div className='rrot-container'>
                 <div className="wrapper">
                 {children}
