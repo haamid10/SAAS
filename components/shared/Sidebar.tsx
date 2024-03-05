8881d1e2f5c772ec73b53827922701ee3e258d38
haamid10 <apdixamiid919@gmail.com>
2024-03-05 15:13:32 +0300

    2

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,7 +1,11 @@
 import  Link  from "next/link"
 import Image from "next/image"
+import { SignedIn } from "@clerk/nextjs"
+import { usePathname } from "next/navigation"
+import { navLinks } from "@/constants"
 
 const Sidebar = () => {
+  const Pathname = usePathname
   return (
     <aside className='sidebar'>
       <div className='flex size-full flex-col gap-4 '>
@@ -9,6 +13,22 @@ const Sidebar = () => {
         <Link href="/" className="sidebar-logo">
           <Image src="/assets/images/logo-text.svg"  alt="logo" width={120} height={120}/>
         </Link>
+
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive =link.route === Pathname;
+                return (
+                  <li key={link.route} className={`sidebar-nav-elements group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
+
+                  </li>
+                )
+              })}
+            </ul>
+          </SignedIn>
+
+        </nav>
       </div>
 
     </aside>
