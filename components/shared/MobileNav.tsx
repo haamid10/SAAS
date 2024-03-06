230dd79a1746f6c0ef3d67cc20f79b019720aa82
haamid10 <apdixamiid919@gmail.com>
2024-03-06 12:00:15 +0300

    installed new shadcn button and intialised

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 
 const MobileNav = () => {
   return (
