ec97d622fc54cf2e23288ca38b359daa9c76ee9e
haamid10 <apdixamiid919@gmail.com>
2024-03-07 16:49:36 +0300

    1

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -6,11 +6,16 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import Image from "next/image"
+import Link from "next/link"
 
 
 const MobileNav = () => {
   return (
     <header className="header">
+      <Link href="/" className="flex  items-center gap-2 md:py-2">
+        <Image src="/assets/images/logo-text.svg" alt="logo" />
+      </Link>
     
     </header>
   )
