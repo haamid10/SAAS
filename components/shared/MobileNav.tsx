6950b500e27c7df8f4a57c247039b67d9f311ccd
haamid10 <apdixamiid919@gmail.com>
2024-03-07 16:51:57 +0300

    add

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -14,7 +14,7 @@ const MobileNav = () => {
   return (
     <header className="header">
       <Link href="/" className="flex  items-center gap-2 md:py-2">
-        <Image src="/assets/images/logo-text.svg" alt="logo" />
+        <Image src="/assets/images/logo-text.svg" alt="logo"  width={120} height={120}/>
       </Link>
     
     </header>
