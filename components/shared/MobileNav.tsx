d6cd3354beb1519d84d1a3188ec2a8f109df0a37
haamid10 <apdixamiid919@gmail.com>
2024-03-10 10:50:08 +0300

    2

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -20,6 +20,18 @@ const MobileNav = () => {
       <nav>
         <SignedIn>
           <UserButton afterSignOutUrl="/" />
+          <Sheet>
+            <SheetTrigger>Open</SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                  This action cannot be undone. This will permanently delete your account
+                  and remove your data from our servers.
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
         </SignedIn>
       </nav>
     
