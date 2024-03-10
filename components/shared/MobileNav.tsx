9d367397e53de0f24eb1733f8a8e1b7e51881233
haamid10 <apdixamiid919@gmail.com>
2024-03-10 10:46:54 +0300

    1

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -6,6 +6,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import { SignedIn, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -14,8 +15,14 @@ const MobileNav = () => {
   return (
     <header className="header">
       <Link href="/" className="flex  items-center gap-2 md:py-2">
-        <Image src="/assets/images/logo-text.svg" alt="logo"  width={120} height={120}/>
+        <Image src="/assets/images/logo-text.svg" alt="logo"  width={180} height={28}/>
       </Link>
+      <nav>
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+      </nav>
+    
     
     </header>
   )
