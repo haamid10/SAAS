ac7e932f4bec763f7f500a137ed5d1d3693922f5
haamid10 <apdixamiid919@gmail.com>
2024-03-10 12:19:25 +0300

    mobile nav

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,17 +1,15 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+"use client"
+import {Sheet,SheetContent,SheetTrigger} from "@/components/ui/sheet"
+import { navLinks } from "@/constants"
 import { SignedIn, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { usePathname } from 'next/navigation'
+
 
 
 const MobileNav = () => {
+  const Pathname  = usePathname();
   return (
     <header className="header">
       <Link href="/" className="flex  items-center gap-2 md:py-2">
@@ -23,6 +21,23 @@ const MobileNav = () => {
           <Sheet>
             <SheetTrigger>
               <Image src="/assets/icons/menu.svg" alt="menu" height={32} width={32} className=" cursor-pointer"/> 
+              <ul className="header-nav_elements">
+                    {navLinks.slice(0,6).map((link) => {
+                      const isActive = link.route === Pathname;
+                      return (
+                        <li key={link.route} className={`sidebar-nav_element  rounded-full px-2 group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
+                          <Link href={link.route} className="sidebar-link">
+                          <Image src={link.icon} alt={link.label} width={24} height={24} className={`${isActive && 'brightness-200'}`}/>
+                          {link.label}
+
+                          </Link>
+                          
+
+                        </li>
+                      )
+                    })}
+                    </ul>
+                    
             </SheetTrigger>
             <SheetContent  className="sheet-content sm:w-64">
               <>
